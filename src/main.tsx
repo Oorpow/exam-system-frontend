7cfdee24a866cd7776f70e1f23395b07b0197fd9
Oorpow <309754513@qq.com>
2024-10-28 10:06:28 +0800

    init: 🎉 init

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter, RouteObject } from 'react-router-dom'
+import './index.css'
+import Login from './pages/Login'
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>home</div>,
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+]
+
+const router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<RouterProvider router={router} />)
